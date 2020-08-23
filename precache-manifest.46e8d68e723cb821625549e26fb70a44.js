e592fc6f4a54a5dab3f85dc9ec41047eb82139d5
magnusson <me@magnusson.dev>
2020-08-23 22:07:52 +0200

    Updates

---
+++ precache-manifest.46e8d68e723cb821625549e26fb70a44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76686fe59c69a33e2660e7a68a2c390a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ea112807962e39ecbfe",
+    "url": "/static/js/2.5f974e16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f974e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae92dff1c2586a901b61",
+    "url": "/static/js/main.e49e0558.chunk.js"
+  },
+  {
+    "revision": "0f28296e3f255f2c9af9",
+    "url": "/static/js/runtime-main.601cbe93.js"
+  }
+]);
